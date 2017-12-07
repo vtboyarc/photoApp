f2fbe2b1c11f022f24907a11cf79ac5b78f3bb05
vtboyarc <adamrcarter@gmail.com>
2017-12-06 19:42:06 -0600

    First commit of photo app

---
+++ Photo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Photo+CoreDataClass.swift
+//  Photorama
+//
+//  Created by Carter, Adam on 7/3/17.
+//  Copyright © 2017 Carter, Adam. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Photo)
+public class Photo: NSManagedObject {
+
+}
---
+++ Photo+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+//
+//  Photo+CoreDataProperties.swift
+//  Photorama
+//
+//  Created by Carter, Adam on 7/3/17.
+//  Copyright © 2017 Carter, Adam. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Photo {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Photo> {
+        return NSFetchRequest<Photo>(entityName: "Photo")
+    }
+
+    @NSManaged public var photoID: String?
+    @NSManaged public var title: String?
+    @NSManaged public var dateTaken: NSDate?
+    @NSManaged public var remoteURL: NSURL?
+
+}
